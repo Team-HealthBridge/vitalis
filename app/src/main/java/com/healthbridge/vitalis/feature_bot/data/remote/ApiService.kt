8a9f78c70388e6fac46e9ddcd387734bee7f8c70
Terry Mochire <terrybmochire@gmail.com>
2023-03-31 01:50:52 +0300

    Add methods to get and refresh tokens.

--- app/src/main/java/com/healthbridge/vitalis/feature_bot/data/remote/ApiService.kt
+++ app/src/main/java/com/healthbridge/vitalis/feature_bot/data/remote/ApiService.kt
@@ -1,20 +1,36 @@
 package com.healthbridge.vitalis.feature_bot.data.remote
 
+import com.healthbridge.vitalis.feature_bot.data.remote.model.RequestToken
 import com.healthbridge.vitalis.feature_bot.data.remote.model.UserActivity
 import com.healthbridge.vitalis.feature_bot.data.remote.responses.ReceiveActivities
 import com.healthbridge.vitalis.feature_bot.data.remote.responses.SendActivity
 import com.healthbridge.vitalis.feature_bot.data.remote.responses.StartConverstion
+import com.healthbridge.vitalis.feature_bot.data.remote.responses.Token
+import com.healthbridge.vitalis.util.Constants
 import kotlinx.coroutines.flow.Flow
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface ApiService{
 
+    val token: String
+        get() = Constants.TOKEN
+
+    // GET TOKEN
+    @POST("/v3/directline/tokens/generate")
+    suspend fun getToken(
+        @Body requestToken: RequestToken,
+    ): Token
+
+    // REFRESH TOKEN
+    @POST("/v3/directline/tokens/refresh")
+    suspend fun refreshToken(
+    ): Token
+
+
     // START CONVERSATION
     @POST("/v3/directline/conversations")
-    suspend fun startConversation(): Flow<StartConverstion>
+    suspend fun startConversation(
+    ): StartConverstion
 
     // SEND ACTIVITY
     @POST("/v3/directline/conversations/{conversationId}/activities")
