e47d09f3ec6eba593db85bd0f3ee21b16cd7e083
Terry Mochire <terrybmochire@gmail.com>
2023-03-31 02:00:12 +0300

    Add functions to get and refresh tokens.

--- app/src/main/java/com/healthbridge/vitalis/feature_bot/data/repository/ChatRepository.kt
+++ app/src/main/java/com/healthbridge/vitalis/feature_bot/data/repository/ChatRepository.kt
@@ -1,16 +1,27 @@
 package com.healthbridge.vitalis.feature_bot.data.repository
 
 import com.healthbridge.vitalis.feature_bot.data.remote.ApiService
+import com.healthbridge.vitalis.feature_bot.data.remote.model.RequestToken
 import com.healthbridge.vitalis.feature_bot.data.remote.model.UserActivity
 import com.healthbridge.vitalis.feature_bot.data.remote.responses.ReceiveActivities
+import com.healthbridge.vitalis.feature_bot.data.remote.responses.Token
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ChatRepository @Inject constructor(private val api: ApiService) {
 
-    suspend fun startConversation(): Flow<String> {
-        return api.startConversation().map { it.conversationId }
+
+    suspend fun getToken(requestToken: RequestToken): Token {
+        return api.getToken(requestToken)
+    }
+
+    suspend fun refreshToken(): Token {
+        return api.refreshToken()
+    }
+
+    suspend fun startConversation(): String {
+        return api.startConversation().conversationId
     }
 
     suspend fun sendActivity(conversationId: String, userActivity: UserActivity): String {
