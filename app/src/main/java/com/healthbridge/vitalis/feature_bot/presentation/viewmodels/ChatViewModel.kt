bdb85cd32bdfa7b8ad836b14ea8adf9e6f89fbb0
Terry Mochire <terrybmochire@gmail.com>
2023-03-31 02:02:09 +0300

    Get Token, start conversation and change Dispatchers.

--- app/src/main/java/com/healthbridge/vitalis/feature_bot/presentation/viewmodels/ChatViewModel.kt
+++ app/src/main/java/com/healthbridge/vitalis/feature_bot/presentation/viewmodels/ChatViewModel.kt
@@ -6,10 +6,13 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.healthbridge.vitalis.feature_bot.data.remote.model.RequestToken
+import com.healthbridge.vitalis.feature_bot.data.remote.model.User
 import com.healthbridge.vitalis.feature_bot.data.remote.model.UserActivity
 import com.healthbridge.vitalis.feature_bot.data.remote.model.UserFrom
 import com.healthbridge.vitalis.feature_bot.data.repository.ChatRepository
 import com.healthbridge.vitalis.feature_bot.di.AppModule
+import com.healthbridge.vitalis.util.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,12 +22,35 @@ import javax.inject.Inject
 @HiltViewModel
 class ChatViewModel @Inject constructor(private val chatRepository: ChatRepository, savedStateHandle: SavedStateHandle) : ViewModel() {
 
-    private val conversationId: String = "CeigNsAD8QpLXCI2tAHW84-fr"
+    private var conversationId: String? = "2APOxIvRhaeJySA8QzUhHg-fr"
+
+    fun getToken() {
+        CoroutineScope(Dispatchers.Main).launch {
+            if ( Constants.TOKEN.length > 55) {
+                Constants.TOKEN = chatRepository.refreshToken().token
+            } else{
+                val requestToken = RequestToken(
+                    user = User(
+                        id = "user2",
+                        name = "HealthBridge"
+                    )
+                )
+                Constants.TOKEN = chatRepository.getToken(requestToken).token
+            }
+        }
+    }
+
+    fun startConversation() {
+        CoroutineScope(Dispatchers.Default).launch {
+           conversationId = chatRepository.startConversation()
+            println(conversationId)
+        }
+    }
 
     fun sendUserInput(input: String) {
-        CoroutineScope(Dispatchers.Default).launch {
+        CoroutineScope(Dispatchers.IO).launch {
             val userActivity = UserActivity(
-                from = UserFrom(id = "user1"),
+                from = UserFrom(id = "Terry Mochire"),
                 locale = "en-US",
                 text = input,
                 type = "message"
@@ -35,11 +61,10 @@ class ChatViewModel @Inject constructor(private val chatRepository: ChatReposito
     }
 
     val botResponse = {
-        CoroutineScope(Dispatchers.Default).launch {
+        CoroutineScope(Dispatchers.IO).launch {
             chatRepository.receiveActivities(conversationId!!)
         }
     }
-
 
 
 }
