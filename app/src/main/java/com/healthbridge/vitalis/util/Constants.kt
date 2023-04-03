bddfdb1bbcc39e27a7470f528c0175b3c08b73f4
Terry Mochire <terrybmochire@gmail.com>
2023-04-03 22:53:04 +0300

    Commit the constants file.

---
+++ app/src/main/java/com/healthbridge/vitalis/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.healthbridge.vitalis.util
+
+object Constants {
+    const val BASE_URL = "https://directline.botframework.com"
+    var TOKEN = "Bearer VejkbWWGYBs.PhU2wzDuk5zuTzIyxGd7zX3fOFB4MwSYhuRisz_wq3g"
+}
