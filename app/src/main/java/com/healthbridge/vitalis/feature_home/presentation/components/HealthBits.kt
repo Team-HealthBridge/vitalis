ec86139b284b12b4e239423d93ef2b1bfaf036af
Terry Mochire <terrybmochire@gmail.com>
2023-04-03 20:18:54 +0300

    Start the base for the home screen.

---
+++ app/src/main/java/com/healthbridge/vitalis/feature_home/presentation/components/HealthBits.kt
@@ -0,0 +1,11 @@
+package com.healthbridge.vitalis.feature_home.presentation.components
+
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HealthBits() {
+
+
+}
