943ce7ca881e4babd39e36c10cbf230f922924b9
Terry Mochire <terrybmochire@gmail.com>
2023-04-12 19:26:21 +0300

    Add functions to follow community, check if a member is following a community and to add a new post.

--- app/src/main/java/com/healthbridge/vitalis/feature_communities/presentation/viewmodels/CommunityViewModel.kt
+++ app/src/main/java/com/healthbridge/vitalis/feature_communities/presentation/viewmodels/CommunityViewModel.kt
@@ -1,17 +1,22 @@
 package com.healthbridge.vitalis.feature_communities.presentation.viewmodels
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.healthbridge.vitalis.feature_communities.data.models.Community
+import com.healthbridge.vitalis.feature_communities.data.models.Member
 import com.healthbridge.vitalis.feature_communities.data.models.Post
 import com.healthbridge.vitalis.feature_communities.data.repository.CommunityRepository
+import kotlinx.coroutines.launch
 
 
 class CommunityViewModel constructor(communityRepository: CommunityRepository) : ViewModel() {
 
     val communitiesResponse: MutableState<List<Community>> = communityRepository.allComunities
     val postResponse: MutableState<List<Post>> = communityRepository.post
+    val isMember : MutableState<Boolean> = mutableStateOf(false)
 
 
     init {
@@ -24,6 +29,24 @@ class CommunityViewModel constructor(communityRepository: CommunityRepository) :
         communityRepository.getPost(string)
     }
 
+    fun followCommunity(communityName: String, member: Member){
+        val communityRepository = CommunityRepository()
+        communityRepository.followCommunity(communityName, member)
+    }
+
+    fun isMember(member: Member, communityName: String){
+        viewModelScope.launch {
+            val communityRepository = CommunityRepository()
+            isMember.value = communityRepository.isCommunityMember(communityName, member)
+        }
+    }
+
+    fun addPost(communityName: String, post: Post){
+        viewModelScope.launch {
+            val communityRepository = CommunityRepository()
+            communityRepository.addPost(communityName, post)
+        }
+    }
 }
 
 class CommunityViewModelFactory(private val repository: CommunityRepository) : ViewModelProvider.Factory {
