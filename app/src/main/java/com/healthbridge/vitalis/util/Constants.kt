75d67b74570c35ab6c4e3790a159a0698e04cde9
Terry Mochire <terrybmochire@gmail.com>
2023-04-14 23:30:34 +0300

    Change bot instance to use bot team's instance.

--- app/src/main/java/com/healthbridge/vitalis/util/Constants.kt
+++ app/src/main/java/com/healthbridge/vitalis/util/Constants.kt
@@ -2,5 +2,6 @@ package com.healthbridge.vitalis.util
 
 object Constants {
     const val BASE_URL = "https://directline.botframework.com"
-    var TOKEN = "Bearer VejkbWWGYBs.PhU2wzDuk5zuTzIyxGd7zX3fOFB4MwSYhuRisz_wq3g"
+    const val BACKEND_URL = "https://vitalis-backend.test.azuremicroservices.io/vitalis/default/api/v1/"
+    var TOKEN = "Bearer 10xV5X_ctm8.81BWv-AlIwryWkZzmSecB4nPneIvc7HEKLF0mOGaqyE"
 }
